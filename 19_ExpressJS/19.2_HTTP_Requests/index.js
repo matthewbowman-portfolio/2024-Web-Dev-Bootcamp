bcfab09cbb2fd9b30eceef6d1a875cf2f54475fb
Matthew Bowman <matthewbowman.portfolio@gmail.com>
2024-09-15 00:59:42 -0400

    Multi Folder - Update

--- 19_ExpressJS/19.2_HTTP_Requests/index.js
+++ 19_ExpressJS/19.2_HTTP_Requests/index.js
@@ -11,9 +11,15 @@ const port = 3000;
 
 //get root "/"
 app.get("/", (req, res) => {
-    console.log("Request Details:");
-    console.log(req.rawHeaders);
-    res.send("Hello World!~~");
+    console.log("\n----------------------------------------------------------------------------------------------------\n");
+    console.log(`Request made : ${new Date()}\n`);
+    console.log(req.headers);
+    res.sendFile("C:/Users/machu/Portfolio_Directory/2024-Web-Dev-Bootcamp/19_ExpressJS/19.2_HTTP_Requests/index.html");
+})
+
+//get "/download"
+app.get("/download", (req, res) => {
+    res.download("C:/Users/machu/Portfolio_Directory/2024-Web-Dev-Bootcamp/19_ExpressJS/19.2_HTTP_Requests/index.js");
 })
 
 //get "/about"
@@ -26,7 +32,8 @@ app.get("/contact", (req, res) => {
     res.send("You can reach me at 555-555-5555");
 })
 
-//listen function with port configuration
+//set listen function and configure port
 app.listen(3000, () => {
-    console.log(`Server running on Port ${port}`);
+    console.log("\n----------------------------------------------------------------------------------------------------\n");
+    console.log(`Server Running on Port ${port}`);
 })
